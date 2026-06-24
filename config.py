649d0139b1b122544a607c1de20f1ee4ffbed982
asilbwxv <alexandre.santos.silva@inesc-id.pt>
2026-06-24 16:30:09 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+JIRA_BASE_URL = "https://cisas.eu.airbus.corp:8343/1V32-jira"
+JIRA_TOKEN = "MTc5NDU1MzU0NTE0OnrZeM3tRaBlXJPW5RaP1njINC5d"
+
+GITHUB_API_URL = "https://gheprivate.intra.corp/api/v3"
+GITHUB_TOKEN = "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"
+GITHUB_ORG = "software-avionics"
+
+ARTIFACTORY_BASE = "https://cisas.eu.airbus.corp:8643/artifactory"
+ARTIFACTORY_AUTH = ("asilbwxv", "cmVmdGtuOjAxOjE3OTA4OTc0ODE6ZE1jNWx2aWhnSGNjT0hnUFhmTjJNSjBzR1lN")
+
+REQUEST_DELAY = 0.1 # To prevent rate limiting
+
+ARTIFACTORY_URL = "https://cisas.eu.airbus.corp:8643/artifactory"
+ARTIFACTORY_USER = "asilbwxv"
+ARTIFACTORY_TOKEN = "cmVmdGtuOjAxOjE3OTczNzEyMzg6WW5ZblFpQ0M4emVjREdqVHpYOXhmcFhtS09o"
